9e3255630d05a316a3e8b942872bc90b69b87e51
wrkode <william.rizzo@suse.com>
2023-10-03 18:54:18 +0200

    improve configmap creation

--- app/go.mod
+++ app/go.mod
@@ -2,7 +2,11 @@ module github.com/wrkode/rancher-selector
 
 go 1.20
 
-require k8s.io/client-go v0.28.2
+require (
+	k8s.io/api v0.28.2
+	k8s.io/apimachinery v0.28.2
+	k8s.io/client-go v0.28.2
+)
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
@@ -34,8 +38,6 @@ require (
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
-	k8s.io/api v0.28.2 // indirect
-	k8s.io/apimachinery v0.28.2 // indirect
 	k8s.io/klog/v2 v2.100.1 // indirect
 	k8s.io/kube-openapi v0.0.0-20230717233707-2695361300d9 // indirect
 	k8s.io/utils v0.0.0-20230406110748-d93618cff8a2 // indirect
--- app/main.go
+++ app/main.go
@@ -1,26 +1,14 @@
 package main
 
 import (
-	"context"
 	"encoding/json"
+	"github.com/wrkode/rancher-selector/cmd"
 	"log"
 	"net/http"
-	"os"
-	"time"
-
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
-	"k8s.io/client-go/util/retry"
 )
 
-type ProjectEvent struct {
-	Namespace string `json:"namespace"`
-	Name      string `json:"name"`
-}
-
 func main() {
-	http.HandleFunc("/api/notify", handler)
+	http.HandleFunc("/", handler)
 
 	log.Println("Server starting on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
@@ -32,45 +20,16 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	var event ProjectEvent
+	var event cmd.ProjectEvent
 	if err := json.NewDecoder(r.Body).Decode(&event); err != nil {
 		http.Error(w, "Invalid JSON", http.StatusBadRequest)
 		return
 	}
 
-	if err := triggerCronJob(os.Getenv("CRONJOB_NAME"), "kube-system"); err != nil {
-		http.Error(w, "Failed to trigger cronjob", http.StatusInternalServerError)
+	err := cmd.CreateOrUpdateConfigMap(event, "rancher-data", "kube-system")
+	if err != nil {
+		log.Printf("Error creating or updating ConfigMap: %v", err)
+		http.Error(w, "Failed to create or update ConfigMap", http.StatusInternalServerError)
 		return
 	}
-
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("CronJob triggered successfully"))
-}
-
-func triggerCronJob(name, namespace string) error {
-	config, err := rest.InClusterConfig()
-	if err != nil {
-		return err
-	}
-
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		return err
-	}
-
-	return retry.RetryOnConflict(retry.DefaultRetry, func() error {
-		cronJob, err := clientset.BatchV1beta1().CronJobs(namespace).Get(context.TODO(), name, metav1.GetOptions{})
-		if err != nil {
-			return err
-		}
-
-		// Trigger the cronjob by modifying its annotations
-		if cronJob.ObjectMeta.Annotations == nil {
-			cronJob.ObjectMeta.Annotations = map[string]string{}
-		}
-		cronJob.ObjectMeta.Annotations["kubectl.kubernetes.io/restartedAt"] = time.Now().Format(time.RFC3339)
-
-		_, updateErr := clientset.BatchV1beta1().CronJobs(namespace).Update(context.TODO(), cronJob, metav1.UpdateOptions{})
-		return updateErr
-	})
 }
